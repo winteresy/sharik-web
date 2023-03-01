a03447887ada3f625931acb3d04ad5818a6f4614
winteresy <alisagurova2@gmail.com>
2023-03-01 14:54:54 +0300

    2 gis

--- sharik-app/src/App.js
+++ sharik-app/src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import "./App.css";
 import PostsContainer from "./components/PostsContainer";
 
+
+
+
+
 class App extends Component {
   render() {
     return (
---
+++ sharik-app/src/components/Map.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { load } from '@2gis/mapgl';
+import { MapWrapper } from './MapWrapper';
+
+
+export const Map = () => {
+    useEffect(() => {
+        let map;
+        load().then((mapglAPI) => {
+            map = new mapglAPI.Map('map-container', {
+                center: [55.31878, 25.23584],
+                zoom: 13,
+                key: '827dd017-d569-4c30-ab73-c12145b62bcd',
+            });
+        });
+
+        // Удаляем карту при размонтировании компонента
+        return () => map && map.destroy();
+    }, []);
+
+    return (
+        <div style={{ width: '100%', height: '100%' }}>
+            <MapWrapper />
+        </div>
+    );
+};
