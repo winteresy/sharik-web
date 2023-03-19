da9aa085ae490efac31ee20f57c738b1216067d9
winteresy <alisagurova2@gmail.com>
2023-03-19 21:11:56 +0300

    new

--- sharik-app/src/components/MapWrapper.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const MapWrapper = React.memo(
-    () => {
-        return <div id="map-container" style={{ width: '100%', height: '100%' }}></div>;
-    },
-    () => true,
-);
